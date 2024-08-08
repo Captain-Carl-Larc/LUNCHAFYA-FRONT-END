e2f8d90eae9d4b2f05cf88cf6ce38f5cd78c3acf
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-08-08 11:33:13 +0300

    making changes in footer

---
+++ partials/footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foooter</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <footer>
+        <div class="contact-us">
+
+        </div>
+        <div class="links-cont">
+            <div id="shortcuts">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Foods</a>
+                    </li>
+                    <li>
+                        <a href="#">Prices</a>
+                    </li>
+                    <li>
+                        <a href="#">Orders</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="more-links">
+                <ul>
+                    <li>
+                        <a href="#">Support</a>
+                    </li>
+                    <li>
+                        <a href="#">Reviews</a>
+                    </li>
+                    <li>
+                        <a href="#">Feedback</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="copyrights">
+            
+        </div>
+    </footer>
+</body>
+</html>
